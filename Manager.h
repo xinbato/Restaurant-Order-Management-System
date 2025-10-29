f20d1b181a8e620f3f3c2b097b36dabd3ca7f787
xinbato <duongdat.20061103@gmail.com>
2025-10-29 19:17:03 +0700

    Add files via upload

---
+++ Manager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "User.h"
+#include <string>
+using namespace std;
+
+class Manager  {
+public:
+	void manageEmployees();
+	void manageMenu();
+	void manageOrders();
+	void generateReports();
+};
